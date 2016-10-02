faa1a57f43a34a4382807fefb4e58fe6793d60f0
Justin Doak <justinhdoak@gmail.com>
2016-10-02 19:40:39 -0400

    Wrote seeds.rb. Set .ruby-version in rbenv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Running seeds!"
+
+Tool.delete_all()
+Gardener.delete_all()
+
+gardeners = Gardener.create!([
+  {
+    email: "joe@ga.co",
+    password: "test1234"
+  }
+])
+
+puts gardeners
+
+tools = Tool.create!([
+  {
+    name: "mower",
+    details: "Old school push mower",
+    gardener: gardeners[0]
+  }
+])
+
+puts tools
