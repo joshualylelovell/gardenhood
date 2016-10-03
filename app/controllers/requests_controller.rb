b180848cefca8307bea6f3dd758e156aa6f7648d
Joshua Lovell <joshualylelovell@gmail.com>
2016-10-03 14:39:49 -0400

    added routes and views for Requests

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,67 @@
+class RequestsController < ApplicationController
+
+  before_action :authenticate_gardener!
+  before_action :set_request, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @requests = Request.all
+  end
+
+  def show
+    @request = Request.find(params[:id])
+  end
+
+  def new
+    @request = Request.new
+    @request.tool = Tool.find(params[:tool])
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.gardener = current_gardener
+    respond_to do |format|
+      if @request.save
+        format.html { redirect_to requests_path, notice: 'Your request has been saved.' }
+        format.json { render :show, status: :created, location: @request }
+      else
+        format.html { render :new }
+        format.json { render json: @request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @request.update(request_params)
+        format.html { redirect_to @request, notice: 'Your request has been updated.' }
+        format.json { render :show, status: :ok, location: @request }
+      else
+        format.html { render :edit }
+        format.json { render json: @request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @request.destroy
+    respond_to do |format|
+      format.html { redirect_to requests_index_url, notice: 'Your request is gone.' }
+      format.json { head :no_content }
+    end
+  end
+
+  # Use callbacks to share common setup or constraints.
+  def set_request
+    @request = Request.find(params[:id])
+    redirect_to root_url, notice: 'Access Denied!' unless current_gardener.id == @request.gardener.id
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def request_params
+    params.require(:request).permit(:start_date, :end_date, :notes, :status, :tool_id)
+  end
+
+end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,13 @@ class Request < ApplicationRecord
 
   belongs_to :gardener
   belongs_to :tool
+
+  # # After initialization, set default values
+  # after_initialize :set_default_values
+  #
+  # def set_default_values
+  #   # Only set if status IS NOT set
+  #   request.status ||= "pending"
+  # end
+
 end
