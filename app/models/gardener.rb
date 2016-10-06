b2db56424d980f1e1e212710033ebd84d83dcebc
Joshua Lovell <joshualylelovell@gmail.com>
2016-10-06 13:09:06 -0400

    added validations to email and username. attempted to change routing of devise sign-up but lost functionality of devise error messages

--- app/models/gardener.rb
+++ app/models/gardener.rb
@@ -4,9 +4,12 @@ class Gardener < ApplicationRecord
   has_many :requests, dependent: :destroy
 
    validates :username, length: { maximum: 16 }
+   validates :username, uniqueness: true
+   validates :email, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
