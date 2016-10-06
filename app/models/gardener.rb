275a4cda037bdefc5cd2d2e9f20745c0b1241703
Joshua Lovell <joshualylelovell@gmail.com>
2016-10-06 10:54:28 -0400

    added username and address to the gardener model

--- app/models/gardener.rb
+++ app/models/gardener.rb
@@ -2,7 +2,9 @@ class Gardener < ApplicationRecord
 
   has_many :tools, dependent: :destroy
   has_many :requests, dependent: :destroy
-  
+
+   validates :username, length: { maximum: 16 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
